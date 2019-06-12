a8da84201135735ac66504913f88cd45d9154972
ilexwg <ilexwg@gmail.com>
2019-06-12 22:39:14 +0800

    feat: save and read note from localStorage

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 new Vue({
   el: '#notebook',
   data: {
-    content: 'This is a note.'
+    content: ''
   },
   computed: {
     notePreview() {
       return marked(this.content);
     }
+  },
+  watch: {
+    content: 'saveNote'
+  },
+  methods: {
+    saveNote(val) {
+      localStorage.setItem('content', val);
+    }
+  },
+  created() {
+    this.content = localStorage.getItem('content') || 'You can write in **markdown**';
   }
 });
